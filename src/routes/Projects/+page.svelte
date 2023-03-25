7f9ac0c1ce02aaf845bc5d1b7e9ae8ce9ff74769
Popeblack2 <69740780+Popeblack2@users.noreply.github.com>
2023-03-25 22:43:24 +0100

    8 files added, 2 deleted

---
+++ src/routes/Projects/+page.svelte
@@ -0,0 +1,29 @@
+<svelte:head>
+	<title>About</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>About Popeblack</h1>
+
+	<p>
+	Popeblack (Identity is a mystery) is a Leader, Community Manager and Ambassador with passion for privacy IRL and Web 3, A graphics designer and an inspiring thought provoker (Live, Learn and Impact)
+
+Popeblack is the Lead for Secret Network Africa and Global Agent Coordinator Secret Network
+
+He's the founder of IRLWeb3 
+
+A community that educate people on Blockchain and Cryptocurrency in real life, inspiring people to Live, Learn and Impact. Organizing workshops and events to help people evolve with the technology and real life use cases.
+
+	</p>
+
+
+	<pre>npm create svelte@latest</pre>
+
+	<p>
+		The page you're looking at is purely static HTML, with no client-side interactivity needed.
+		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
+		the devtools network panel and reloading.
+	</p>
+</div>
+
